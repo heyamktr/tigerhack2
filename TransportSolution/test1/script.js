a90e7d67c4838b14b1ebff0ce9e0623988d66024
Truong Nguyen Tien Khanh <88968349+khanhtruongloveu@users.noreply.github.com>
2024-12-22 17:58:56 -0500

    Create script.js

---
+++ TransportSolution/test1/script.js
@@ -0,0 +1,9 @@
+// Function to display the popup
+function showPopup() {
+    document.getElementById('popup').style.display = 'block';
+}
+
+// Function to hide the popup
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
