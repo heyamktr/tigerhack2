f717856b71b5b4a31989bf0e5225161ce7541db4
Truong Nguyen Tien Khanh <88968349+khanhtruongloveu@users.noreply.github.com>
2025-02-24 02:27:06 -0500

    Delete script.js

--- TransportSolution/test1/script.js
+++
@@ -1,9 +0,0 @@
-// Function to display the popup
-function showPopup() {
-    document.getElementById('popup').style.display = 'block';
-}
-
-// Function to hide the popup
-function closePopup() {
-    document.getElementById('popup').style.display = 'none';
-}
